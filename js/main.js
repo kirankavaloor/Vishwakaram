763a6af1ac672194b67358d0bd8bf13477d3d88b
kiranrb@springlabs.in <kavaloorkiran@gmail.com>
2021-10-17 17:26:55 +0530

    whatsApp errors fixed

--- js/main.js
+++ js/main.js
@@ -6,7 +6,15 @@ $(window).on("load",function(){
 
 })
 
-
+$(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $("button.navbar-toggler").click();
+        }
+    });
+});
 
 $.scrollIt({
     topOffset: -50
